a05b22fdc4185a6cb374fcd8b208f60f32d797be
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2018-03-08 19:23:32 -0300

    React Native Part 4 : Like and comment

--- react-native/InstaluraMobile/components/Post.js
+++ react-native/InstaluraMobile/components/Post.js
@@ -13,27 +13,103 @@ import {
     View,
     Image,
     Dimensions,
-    FlatList
+    FlatList,
+    TouchableOpacity
 } from 'react-native';
 
 const width = Dimensions.get('screen').width;
 
 type Props = {};
 export default class Post extends Component<Props> {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            foto: this.props.foto
+        }
+    }
+
+    carregaIcone(likeada) {
+        return likeada ? 
+                    require('../resources/img/s2-checked.png') :
+                    require('../resources/img/s2.png');
+    }
+
+    like() {
+        const {foto} = this.state;
+
+        let novaLista = [];
+        if (!foto.likeada) {
+            novaLista = [
+                ...foto.likers,
+                {login: 'meuUsuario'}
+            ]
+        } else {
+            novaLista = foto.likers.filter(liker => {
+                return liker.login  !== 'meuUsuario'
+            });
+        }
+
+        const fotoAtualizada = {
+            ...this.state.foto,
+            likeada: !this.state.foto.likeada,
+            likers: novaLista
+        }
+
+        this.setState({foto: fotoAtualizada})
+    }
+
+    exibeLikes(likers) {
+        if (likers.length <= 0)
+            return;
+            
+        return (
+            <Text style={styles.likes}>
+                {likers.length} {likers.length > 1 ? 'curtidas' : 'curtida'}
+            </Text>
+        );
+    }
+
+    exibeLegenda(foto) {
+        if (foto.comentario === '')
+            return;
+        
+        return (
+            <View style={styles.comentario}>
+                <Text style={styles.tituloComentario}>{foto.loginUsuario}</Text>
+                <Text>{foto.comentario}</Text>
+            </View>
+        );
+    }
+
     render() {
+        const { foto } = this.state;
+
         return (
             <View>
                 <View style={styles.cabecalho}>
                     <Image
-                        source={{uri: this.props.foto.urlPerfil}}
+                        source={{uri: foto.urlPerfil}}
                         style={styles.fotoDePerfil}
                     />
-                    <Text>{this.props.foto.loginUsuario}</Text>
+                    <Text>{foto.loginUsuario}</Text>
                 </View>
                 <Image
-                    source={{uri: this.props.foto.urlFoto}}
+                    source={{uri: foto.urlFoto}}
                     style={styles.foto}
                 />
+
+                <View style={styles.rodape}>
+                    <TouchableOpacity onPress={this.like.bind(this)}>
+                        <Image 
+                            style={styles.botaoLike}
+                            source={this.carregaIcone(foto.likeada)} 
+                        />
+                    </TouchableOpacity>
+
+                    {this.exibeLikes(foto.likers)}
+                    {this.exibeLegenda(foto)}
+                </View>
             </View>
         );
     }
@@ -54,5 +130,22 @@ const styles = StyleSheet.create({
     foto: {
         width: width,
         height: width
+    },
+    botaoLike: {
+        height: 40,
+        width: 40
+    },
+    rodape: {
+        margin: 10
+    },
+    likes: {
+        fontWeight: 'bold'
+    },
+    comentario: {
+        flexDirection: 'row'
+    },
+    tituloComentario: {
+        fontWeight: 'bold',
+        marginRight: 5
     }
 });
